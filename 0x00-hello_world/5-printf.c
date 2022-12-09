f76f67aecc49f18d5ae851008d884ab38e6dd920
Godspower-Jones <godspowerjones23@gmail.com>
2022-12-09 01:19:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
